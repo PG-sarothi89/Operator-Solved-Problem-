c9614533328dc1555a1ac0e510c2f67bb982fb43
PsGhosh <pgsarothi2005@gmail.com>
2025-09-08 13:06:01 +0600

    comment

--- code.c
+++ code.c
@@ -13,3 +13,4 @@ int main()
 // output is 5, var is 4
 // Explanation: In C, the sizeof operator is evaluated at compile time and does not evaluate its operand. 
 //Therefore, the expression i++ is not evaluated, and the value of var is the size of the integer type, which is 4 bytes on most systems. The value of i remains 5.
+// according to c17 standard, sizeof is not an expression and does not evaluate its operand.
